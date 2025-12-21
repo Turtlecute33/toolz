c9ca5b279078e01007f639c103f646c093b76ad0
turtlecute33 <1satoshi@riseup.net>
2025-12-21 17:05:06 +0100

    Optimize build configuration with enhanced minification
    - Enable HTML minification in webpack.main.js with comprehensive options
    - Add CSS minification with CssMinimizerPlugin in production build
    - Configure TerserPlugin to remove console.log statements and comments
    - Implement code splitting for vendor bundles
    - Add performance budgets for bundle size monitoring

--- config/webpack.main.js
+++ config/webpack.main.js
@@ -47,7 +47,16 @@ module.exports = {
 					template: `./${page}.ejs`,
 					filename: `${page}.html`,
 					chunks: [page],
-					minify: false,
+					minify: {
+						collapseWhitespace: true,
+						removeComments: true,
+						removeRedundantAttributes: true,
+						removeScriptTypeAttributes: true,
+						removeStyleLinkTypeAttributes: true,
+						useShortDoctype: true,
+						minifyCSS: true,
+						minifyJS: true
+					},
 					sources: false
 				})
 		)
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -2,6 +2,7 @@ const main = require('./webpack.main')
 const { merge } = require('webpack-merge')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 
 module.exports = merge(main, {
 	mode: 'production',
@@ -9,9 +10,44 @@ module.exports = merge(main, {
 		minimize: true,
 		minimizer: [
 			new TerserPlugin({
-				test: /\.js(\?.*)?$/i
+				test: /\.js(\?.*)?$/i,
+				terserOptions: {
+					compress: {
+						drop_console: false,
+						pure_funcs: ['console.log']
+					},
+					output: {
+						comments: false
+					}
+				},
+				extractComments: false
+			}),
+			new CssMinimizerPlugin({
+				minimizerOptions: {
+					preset: [
+						'default',
+						{
+							discardComments: { removeAll: true }
+						}
+					]
+				}
 			})
-		]
+		],
+		splitChunks: {
+			chunks: 'all',
+			cacheGroups: {
+				vendor: {
+					test: /[\\/]node_modules[\\/]/,
+					name: 'vendors',
+					priority: 10
+				}
+			}
+		}
 	},
-	plugins: [new CleanWebpackPlugin()]
+	plugins: [new CleanWebpackPlugin()],
+	performance: {
+		hints: 'warning',
+		maxEntrypointSize: 512000,
+		maxAssetSize: 512000
+	}
 })
